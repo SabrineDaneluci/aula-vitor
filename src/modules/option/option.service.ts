c0a76e783f16bbbb76bd309375090459e9eb0f50
SabrineDaneluci <sabrinedanelucii@gmail.com>
2025-09-29 10:43:02 -0400

    prontinho

--- src/modules/option/option.service.ts
+++ src/modules/option/option.service.ts
@@ -1,31 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOptionDto } from './dto/create-option.dto';
 import { UpdateOptionDto } from './dto/update-option.dto';
-import { CreateOptionUseCase } from './use-cases';
+import { CreateOptionUseCase } from './use-cases/create-option.use-case';
+import { UpdateOptionUseCase } from './use-cases/update-option.use-case';
+import { DeleteOptionUseCase } from './use-cases/delete-option.use-case';
+import { FindOneOptionUseCase } from './use-cases/find-one-option.use-case';
+import { ListOptionUseCase } from './use-cases/list-option.use-case';
 
 @Injectable()
 export class OptionService {
-   constructor(
-      private readonly createOptionUseCase: CreateOptionUseCase,
+  constructor(
+    private readonly createOptionUseCase: CreateOptionUseCase,
+    private readonly listOptionUseCase: ListOptionUseCase,
+    private readonly updateOptionUseCase: UpdateOptionUseCase,
+    private readonly findOneOptionUseCase: FindOneOptionUseCase,
+    private readonly deleteOptionUseCase: DeleteOptionUseCase,
+  ) {}
 
-    ) {}
   create(data: CreateOptionDto) {
     return this.createOptionUseCase.execute(data);
   }
 
   findAll() {
-    return `This action returns all option`;
+    return this.listOptionUseCase.execute();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} option`;
+  findOne(id: string) {
+    return this.findOneOptionUseCase.execute(id);
   }
 
-  update(id: number, updateOptionDto: UpdateOptionDto) {
-    return `This action updates a #${id} option`;
+  update(id: string, data: UpdateOptionDto) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    return this.updateOptionUseCase.execute(id, data);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} option`;
+  remove(id: string) {
+    return this.deleteOptionUseCase.execute(id);
   }
 }
